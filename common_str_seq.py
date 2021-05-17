4874da837bcb49a5571b1349609a5eee4cd695fe
xushunke <xushunke@souche.com>
2021-05-17 14:39:35 +0800

    fc

---
+++ common_str_seq.py
@@ -0,0 +1,6 @@
+def count_str_seq():
+    pass
+
+
+if __name__ == "__main__":
+    print(count_str_seq("fish", "fosh"))
---
+++ common_str_subseq.py
@@ -0,0 +1,6 @@
+def count_str_subseq():
+    pass
+
+
+if __name__ == "__main__":
+    print(count_str_subseq("fish", "fosh"))
