7704b874f64d0d4c43259e9e08d7e8eb73b9dd93
xushunke <xushunke@souche.com>
2021-05-17 14:36:34 +0800

    fc

--- half_divide_search.py
+++ half_divide_search.py
@@ -1,19 +1,19 @@
-def half_devide_search(arr_in_order, target):
+def half_divide_search(arr_in_order, target):
     if len(arr_in_order) == 0:
         return False
-    less_arr, mid_num, greater_arr = half_devide(arr_in_order)
+    less_arr, mid_num, greater_arr = half_divide(arr_in_order)
     if mid_num < target:
-        return half_devide_search(greater_arr, target)
+        return half_divide_search(greater_arr, target)
     elif mid_num > target:
-        return half_devide_search(less_arr, target)
+        return half_divide_search(less_arr, target)
     else:
         return True
 
 
-def half_devide(arr):
+def half_divide(arr):
     mid_inx = int(len(arr) / 2)
     return arr[:mid_inx], arr[mid_inx], arr[mid_inx + 1:]
 
 
 if __name__ == '__main__':
-    print(half_devide_search([1, 3, 4, 5, 7, 88], 5))
+    print(half_divide_search([1, 3, 4, 5, 7, 88], 5))
