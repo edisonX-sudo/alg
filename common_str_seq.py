79903a57c13a00cd3b5793ce2e68f6cdef990d7c
xushunke <xushunke@souche.com>
2021-05-17 15:35:13 +0800

    fc

--- common_str_seq.py
+++ common_str_seq.py
@@ -1,5 +1,18 @@
-def count_str_seq():
-    pass
+def count_str_seq(s1, s2):
+    dp = [[0 for _ in range(len(s2))] for _ in range(len(s1))]
+    max_num = 0
+    for i1 in range(len(s1)):
+        for i2 in range(len(s2)):
+            c1, c2 = s1[i1], s2[i2]
+            if c1 == c2:
+                if i1 - 1 < 0 or i2 - 1 < 0:
+                    dp[i1][i2] = 0
+                dp[i1][i2] = dp[i1 - 1][i2 - 1] + 1
+                max_num = max(max_num, dp[i1][i2])
+            else:
+                dp[i1][i2] = 0
+
+    return max_num
 
 
 if __name__ == "__main__":
--- common_str_subseq.py
+++ common_str_subseq.py
@@ -1,5 +1,15 @@
-def count_str_subseq():
-    pass
+def count_str_subseq(s1, s2):
+    dp = [[0 for _ in range(len(s2))] for _ in range(len(s1))]
+    for i1 in range(len(s1)):
+        for i2 in range(len(s2)):
+            c1, c2 = s1[i1], s2[i2]
+            if c1 == c2:
+                dp[i1][i2] = dp[i1 - 1][i2 - 1] + 1 if i1 - 1 >= 0 and i2 - 1 >= 0 else 1
+            else:
+                t1 = dp[i1 - 1][i2] if i1 - 1 >= 0 else 0
+                t2 = dp[i1][i2 - 1] if i2 - 1 >= 0 else 0
+                dp[i1][i2] = max(t1, t2)
+    return dp[-1][-1]
 
 
 if __name__ == "__main__":
